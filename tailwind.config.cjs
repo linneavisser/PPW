7225f886851c1b210300cb2b733c2a90af09a430
Linnea Visser <anna56c1@stud.kea.dk>
2022-10-20 10:31:49 +0200

    css and js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'myRed' : '#B70000',
+      'offBlack' : '#1F1F1F',
+      'offWhite': '#FCFCFC',
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
