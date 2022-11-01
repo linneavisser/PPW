9314efe915180e1c6afe4a26286ac9003e9d5b05
Linnea Visser <anna56c1@stud.kea.dk>
2022-11-01 13:27:44 +0100

    update links

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // about: resolve(__dirname, "src/about.html"),
+        borsen: resolve(__dirname, "src/borsen.html"),
+        customiseme: resolve(__dirname, "src/customiseme.html"),
+        eurowoman: resolve(__dirname, "src/eurowoman.html"),
+        kraess: resolve(__dirname, "src/kraess.html"),
+       silfen: resolve(__dirname, "src/silfen.html"),
+       styleby: resolve(__dirname, "src/styleby.html"),
+       vildskud: resolve(__dirname, "src/vildskud.html"),
+       vogue: resolve(__dirname, "src/vogue.html"),
       },
     },
   },
